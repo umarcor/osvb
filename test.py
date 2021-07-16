c015f42f30c6e650219ebd4293f0babd9120ffd3
umarcor <unai.martinezcorral@ehu.eus>
2021-07-16 06:17:29 +0200

    test: add license header; run black

--- test.py
+++ test.py
@@ -1,3 +1,23 @@
+# Authors:
+#   Unai Martinez-Corral
+#
+# Copyright 2021 Unai Martinez-Corral <unai.martinezcorral@ehu.eus>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
 """
 Verify that all the tests work
 """
@@ -11,7 +31,7 @@ import unittest
 import pytest
 
 
-isWin = platform == 'win32'
+isWin = platform == "win32"
 
 
 class TestExtended(unittest.TestCase):
@@ -20,40 +40,38 @@ class TestExtended(unittest.TestCase):
     """
 
     def setUp(self):
-        self.shell = [which('bash')] if platform == 'win32' else []
+        self.shell = [which("bash")] if platform == "win32" else []
         self.root = Path(__file__).parent
-        print('\n::group::Log')
+        print("\n::group::Log")
         sys.stdout.flush()
 
     def tearDown(self):
-        print('\n::endgroup::')
+        print("\n::endgroup::")
         sys.stdout.flush()
-
 
     def _sh(self, args):
         check_call(self.shell + args, stderr=STDOUT)
 
     def _tcl(self, args):
-        check_call(['tclsh'] + args, stderr=STDOUT)
+        check_call(["tclsh"] + args, stderr=STDOUT)
 
     def _py(self, args):
         check_call([executable] + args, stderr=STDOUT)
 
-
     def test_AXI4Stream_VUnit(self):
-        self._py([str(self.root / 'AXI4Stream' / 'test' / 'vunit' / 'run.py')])
+        self._py([str(self.root / "AXI4Stream" / "test" / "vunit" / "run.py")])
 
     def test_AXI4Stream_VUnitCAPI(self):
-        self._py([str(self.root / 'AXI4Stream' / 'test' / 'vunit' / 'run_capi.py')])
+        self._py([str(self.root / "AXI4Stream" / "test" / "vunit" / "run_capi.py")])
 
     @pytest.mark.xfail
     def test_AXI4Stream_OSVVM_TCL(self):
-        self._tcl([str(self.root / 'AXI4Stream' / 'test' / 'osvvm' / 'run.pro')])
+        self._tcl([str(self.root / "AXI4Stream" / "test" / "osvvm" / "run.pro")])
 
     @pytest.mark.xfail
     def test_AXI4Stream_OSVVM_VUnit(self):
-        self._py([str(self.root / 'AXI4Stream' / 'test' / 'osvvm' / 'run.py')])
+        self._py([str(self.root / "AXI4Stream" / "test" / "osvvm" / "run.py")])
 
     @pytest.mark.xfail
     def test_SFF_VUnit_cocotb(self):
-        self._py([str(self.root / 'SFF' / 'test' / 'run.py'), '-v'])
+        self._py([str(self.root / "SFF" / "test" / "run.py"), "-v"])
