6075a940f2a300b5041d0676ee0e4b83c69a9532
umarcor <unai.martinezcorral@ehu.eus>
2021-07-16 05:50:54 +0200

    pyCAPI: add license headers; run black

--- mods/pyCAPI/VUnit.py
+++ mods/pyCAPI/VUnit.py
@@ -1,10 +1,26 @@
 # Python API for using CAPI 3 core files from VUnit
 
-def AddCoreFilesets(
-    vunitHandle,
-    core,
-    filesets
-):
+# Authors:
+#   Unai Martinez-Corral
+#
+# Copyright 2021 Unai Martinez-Corral <unai.martinezcorral@ehu.eus>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
+def AddCoreFilesets(vunitHandle, core, filesets):
     """
     Add the sources of some filesets from a CAPI 3 compliant core, into a VUnit
     handle through VUnit's API (``add_library`` and ``add_source_files``).
@@ -15,13 +31,13 @@ def AddCoreFilesets(
     """
     _root = core.FILE_PATH.parent
 
-    _defaultLib = 'VUnitUserLib'
-    _sources = { _defaultLib: [] }
+    _defaultLib = "VUnitUserLib"
+    _sources = {_defaultLib: []}
 
     for fsetname in filesets:
         if fsetname in core.filesets:
             fset = core.filesets[fsetname]
-            _lib = _defaultLib if fset.logical_name == '' else fset.logical_name
+            _lib = _defaultLib if fset.logical_name == "" else fset.logical_name
             if _lib not in _sources:
                 _sources[_lib] = []
             _sources[_lib] += [_root / fset.path / fstr for fstr in fset.files]
--- mods/pyCAPI/__init__.py
+++ mods/pyCAPI/__init__.py
@@ -1,5 +1,25 @@
 # Python Data Model for CAPI 3 core files
 
+# Authors:
+#   Unai Martinez-Corral
+#
+# Copyright 2021 Unai Martinez-Corral <unai.martinezcorral@ehu.eus>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
 from typing import Dict, List, Union
 from pathlib import Path
 from dataclasses import dataclass, field
@@ -11,6 +31,7 @@ class IpCoreConfig(YamlDataClassConfig):
     """
     Each core file must contain an 'ipcore' field with the metadata of the design.
     """
+
     namespace: str = None
     name: str = None
     version: str = None
@@ -21,6 +42,7 @@ class FilesetConfig(YamlDataClassConfig):
     """
     A Fileset is a group of sources of the same type, to be used together.
     """
+
     path: str = field(default_factory=str)
     file_type: str = field(default_factory=str)
     logical_name: str = field(default_factory=str)
@@ -32,18 +54,17 @@ class Config(YamlDataClassConfig):
     """
     Definition of the CAPI 3 format for core configuration files.
     """
+
     CAPI: int = None
 
     ipcore: IpCoreConfig = None
 
     # TODO Should be a Union for supporting a global 'file_type'
-    #filesets: Dict[str, Union[str, FilesetConfig]] = None
+    # filesets: Dict[str, Union[str, FilesetConfig]] = None
     filesets: Dict[str, FilesetConfig] = None
 
 
-def LoadCoreFile(
-    coreFilePath
-):
+def LoadCoreFile(coreFilePath):
     """
     Load a CAPI 3 compliant core configuration file and unmarshal it.
 
@@ -54,17 +75,17 @@ def LoadCoreFile(
     _core.load(_cpath)
     _core.FILE_PATH = _cpath
     print(_core.FILE_PATH)
-    print('CAPI:', _core.CAPI)
-    print('IpCore:')
-    print('  namespace:', _core.ipcore.namespace)
-    print('  name:', _core.ipcore.name)
-    print('  version:', _core.ipcore.version)
-    print('Filesets:')
+    print("CAPI:", _core.CAPI)
+    print("IpCore:")
+    print("  namespace:", _core.ipcore.namespace)
+    print("  name:", _core.ipcore.name)
+    print("  version:", _core.ipcore.version)
+    print("Filesets:")
     for key, val in _core.filesets.items():
-        print(' - %s:' % key)
-        print('   path:', val.path)
-        print('   file_type:', val.file_type)
-        print('   logical_name:', val.logical_name)
-        print('   files:', val.files)
+        print(" - %s:" % key)
+        print("   path:", val.path)
+        print("   file_type:", val.file_type)
+        print("   logical_name:", val.logical_name)
+        print("   files:", val.files)
 
     return _core
