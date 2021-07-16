c64c1efcac933c50d6e366cce1535b1fc38f150a
umarcor <unai.martinezcorral@ehu.eus>
2021-07-16 06:09:21 +0200

    fpconv: add license headers

--- fpconv/vpi_fpconv.c
+++ fpconv/vpi_fpconv.c
@@ -1,4 +1,25 @@
 /*
+* Authors:
+*   Unai Martinez-Corral
+*
+* Copyright 2020-2021 Unai Martinez-Corral <unai.martinezcorral@ehu.eus>
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* SPDX-License-Identifier: Apache-2.0
+*/
+
+/*
 Prototype for creating a frequency distribution of the signals during a simulation.
 At the start of the simulation, the hierarchy of the design is explored and full signal names are read.
 During the simulation, values of the signals are read and the distribution table is updated.
