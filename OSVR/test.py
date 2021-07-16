177b067ba5ca62cab15f60687def5e02a04e0181
umarcor <unai.martinezcorral@ehu.eus>
2021-07-16 06:01:01 +0200

    OSVR: add license header; run black

--- OSVR/test.py
+++ OSVR/test.py
@@ -1,6 +1,28 @@
+#!/usr/bin/env python3
+
+# Authors:
+#   Unai Martinez-Corral
+#
+# Copyright 2021 Unai Martinez-Corral <unai.martinezcorral@ehu.eus>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
 from pathlib import Path
 from pyOSVR import LoadOSVRFile, LoadXUnitFile
 
-OSVR = LoadOSVRFile(Path(__file__).parent / 'vunit.osvr.yml')
+OSVR = LoadOSVRFile(Path(__file__).parent / "vunit.osvr.yml")
 
-OSVR = LoadXUnitFile(Path(__file__).parent / 'vunit.xml')
+OSVR = LoadXUnitFile(Path(__file__).parent / "vunit.xml")
