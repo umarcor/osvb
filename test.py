310b35441137f9a2aabcd33107247f1ecd11c68a
umarcor <unai.martinezcorral@ehu.eus>
2021-10-24 04:55:50 +0200

    test: style

--- test.py
+++ test.py
@@ -22,13 +22,13 @@
 Verify that all the tests work
 """
 
-import sys
+from sys import stdout as sys_stdout
 from sys import executable, platform
 from pathlib import Path
 from subprocess import check_call, STDOUT
 from shutil import which
 import unittest
-import pytest
+from pytest import mark
 
 
 isWin = platform == "win32"
@@ -43,11 +43,11 @@ class TestExtended(unittest.TestCase):
         self.shell = [which("bash")] if platform == "win32" else []
         self.root = Path(__file__).parent
         print("\n::group::Log")
-        sys.stdout.flush()
+        sys_stdout.flush()
 
     def tearDown(self):
         print("\n::endgroup::")
-        sys.stdout.flush()
+        sys_stdout.flush()
 
     def _sh(self, args):
         check_call(self.shell + args, stderr=STDOUT)
@@ -58,20 +58,24 @@ class TestExtended(unittest.TestCase):
     def _py(self, args):
         check_call([executable] + args, stderr=STDOUT)
 
+    # AXI4Stream
+
     def test_AXI4Stream_VUnit(self):
-        self._py([str(self.root / "AXI4Stream" / "test" / "vunit" / "run.py")])
+        self._py([str(self.root / "AXI4Stream/test/vunit/run.py")])
 
     def test_AXI4Stream_VUnitCAPI(self):
-        self._py([str(self.root / "AXI4Stream" / "test" / "vunit" / "run_capi.py")])
+        self._py([str(self.root / "AXI4Stream/test/vunit/run_capi.py")])
 
-    @pytest.mark.xfail
+    @mark.xfail
     def test_AXI4Stream_OSVVM_TCL(self):
-        self._tcl([str(self.root / "AXI4Stream" / "test" / "osvvm" / "run.pro")])
+        self._tcl([str(self.root / "AXI4Stream/test/osvvm/run.pro")])
 
-    @pytest.mark.xfail
+    @mark.xfail
     def test_AXI4Stream_OSVVM_VUnit(self):
-        self._py([str(self.root / "AXI4Stream" / "test" / "osvvm" / "run.py")])
+        self._py([str(self.root / "AXI4Stream/test/osvvm/run.py")])
 
-    @pytest.mark.xfail
+    # SFF
+
+    @mark.xfail
     def test_SFF_VUnit_cocotb(self):
-        self._py([str(self.root / "SFF" / "test" / "run.py"), "-v"])
+        self._py([str(self.root / "SFF/test/run.py"), "-v"])
