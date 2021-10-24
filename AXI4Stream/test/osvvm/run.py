6958c2281044d494bfa543069f2b9f1f8c1f089d
umarcor <unai.martinezcorral@ehu.eus>
2021-10-24 06:46:52 +0200

    AXI4Stream/osvvm: update run.py

--- AXI4Stream/test/osvvm/run.py
+++ AXI4Stream/test/osvvm/run.py
@@ -4,14 +4,28 @@
 from pathlib import Path
 from vunit import VUnit
 
-VU = VUnit.from_argv()
-VU.add_verification_components()
+
+# Can't compile all OSVVM files since some depends on simulator
+# This naive filter compiles everything except Aldec and Cadence related files
+def osvvm_filter_files(path):
+    return [
+        file
+        for file in path.glob("*.vhd")
+        if (not str(file).endswith("_c.vhd") and not str(file).endswith("_Aldec.vhd"))
+    ]
+
 
 ROOT = Path(__file__).parent
+OSVVM = ROOT / "../../../mods/osvvm"
 
-VU.add_library("lib").add_source_files([
-    ROOT / "*.vhd",
-    ROOT / '..' / '..' / "src" / "*.vhd"
-])
+VU = VUnit.from_argv()
+
+VU.add_library("OSVVM").add_source_files(osvvm_filter_files(OSVVM / "osvvm"))
+VU.add_library("OSVVM_Common").add_source_files(OSVVM / "Common/src/*.vhd")
+VU.add_library("OSVVM_AXI4").add_source_files(
+    [OSVVM / f"AXI4/{subdir}/src/*.vhd" for subdir in ["common", "AxiStream", "Axi4"]]
+)
+
+VU.add_library("lib").add_source_files([ROOT / "*.vhd", ROOT / ".." / ".." / "src" / "*.vhd"])
 
 VU.main()
