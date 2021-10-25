181703b38cf6bcbe627938556ca689a49d1ca291
umarcor <unai.martinezcorral@ehu.eus>
2021-10-25 05:59:07 +0200

    ci: clean install-osvvm.sh

--- .github/install-osvvm.sh
+++ .github/install-osvvm.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-OSVVMLibraries="$(dirname $0)/../mods/osvvm/osvvm"
+OSVVM="$(dirname "$0")/../mods/osvvm/osvvm"
 
 # Temporarily revert OSVVM to 2021.06
 # https://github.com/ghdl/ghdl/issues/1900
-cd "$OSVVMLibraries"/..
+cd "$OSVVM"/..
 git checkout -b v2021.06 2021.06
 git submodule update --init --recursive
 cd -
 
 # Currently, the bash variant of GHDL's vendor scripts supports the core of OSVVM only
-$(dirname $(which ghdl))/../lib/ghdl/vendors/compile-osvvm.sh --all --source $(dirname "$0")/../mods/osvvm/osvvm --output precompiled
+$(dirname $(which ghdl))/../lib/ghdl/vendors/compile-osvvm.sh --all --source "$OSVVM" --output precompiled
