8ea7e37de3f464066b081f303183771e9a7cf488
umarcor <unai.martinezcorral@ehu.eus>
2021-10-25 05:59:07 +0200

    ci: temporarily pin OSVVM to 2021.06 when using GHDL's vendor scripts

--- .github/install-osvvm.sh
+++ .github/install-osvvm.sh
@@ -2,4 +2,14 @@
 
 set -e
 
-$(dirname $(which ghdl))/../lib/ghdl/vendors/compile-osvvm.sh --osvvm --source $(dirname $0)/../mods/osvvm/osvvm --output precompiled
+OSVVMLibraries="$(dirname $0)/../mods/osvvm/osvvm"
+
+# Temporarily revert OSVVM to 2021.06
+# https://github.com/ghdl/ghdl/issues/1900
+cd "$OSVVMLibraries"/..
+git checkout -b v2021.06 2021.06
+git submodule update --init --recursive
+cd -
+
+# Currently, the bash variant of GHDL's vendor scripts supports the core of OSVVM only
+$(dirname $(which ghdl))/../lib/ghdl/vendors/compile-osvvm.sh --all --source $(dirname "$0")/../mods/osvvm/osvvm --output precompiled
